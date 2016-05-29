8b397d67e796a073a329f66969dbc6ee4cf651a6
JWNg <JWNg@users.noreply.github.com>
2016-05-29 07:47:28 -0400

    Update Pre-FizzBuzz_Workout#1.rb

--- Pre-FizzBuzz_Workout#1.rb
+++ Pre-FizzBuzz_Workout#1.rb
@@ -1,5 +1,15 @@
-def pre_fizz(n)
+#This is the first step to understanding FizzBuzz.
+#
+#Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+#
+#Your expected outputs: an array of positive integers from 1 to n
+#
+#Your job is to write an algorithm that gets you from the input to the output.
 
+def pre_fizz(n)
+  list = []
+  (1..n).each{|i| list << i}
+  list
 end
 
 #What are the inputs to this function?
