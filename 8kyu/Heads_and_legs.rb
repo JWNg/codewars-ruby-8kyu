3b7b9b6ec0d5439acd367ecef9a81c5b899f7330
JWNg <JWNg@users.noreply.github.com>
2016-06-03 07:53:52 -0400

    Update Heads_and_legs.rb

--- 8kyu/Heads_and_legs.rb
+++ 8kyu/Heads_and_legs.rb
@@ -30,13 +30,15 @@
 #
 #Happy coding!
 
-
 def animals(heads, legs)
+  return [0, 0] if [heads, legs] == [0,0]
   return "No solutions" if (legs % 2) == 1
+  return "No solutions" if heads == 0
   cows, chickens = 0, heads
-  until ((chickens * 2)+(cows * 4)) == legs || cows >= 1000 do
+  until ((chickens * 2)+(cows * 4)) == legs || cows >= 1000 || chickens <= 0 do
     chickens -= 1
     cows += 1
   end
+  return "No solutions" if chickens < 0 || ((chickens * 2)+(cows * 4)) != legs
   [chickens, cows] 
 end
