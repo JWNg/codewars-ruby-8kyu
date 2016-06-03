a9a99d634898736f9a492dfa4a5918f4db4c20c5
Atom <jeffng@Macintosh-0026b0e4de10.local>
2016-06-03 06:39:43 -0400

    update file container for each kyu (1-8)

--- 8kyu/sum_of_positive.rb
+++ 8kyu/sum_of_positive.rb
@@ -1,3 +1,7 @@
 #You get an array of numbers, return the sum of all of the positives ones.
 #
 #Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+def positive_sum(arr)
+  arr.inject(0){|sum, n| n>-1? sum + n : sum }
+end
