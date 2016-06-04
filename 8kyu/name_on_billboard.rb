2c266e5007821499325af8d7d50a1812a8b5d9a4
JWNg <JWNg@users.noreply.github.com>
2016-06-04 12:37:37 -0400

    Update name_on_billboard.rb

--- 8kyu/name_on_billboard.rb
+++ 8kyu/name_on_billboard.rb
@@ -5,3 +5,11 @@
 #You can not use multiplier "*" operator.
 #
 #If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a letter).
+
+def billboard(name, price=30)
+  counter = 0
+  for i in 1..(name.length) 
+    counter += price
+  end
+  counter
+end
