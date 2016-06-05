df0dced7dbe4bcce47d941784302ab1a650edd4c
JWNg <JWNg@users.noreply.github.com>
2016-06-05 08:10:00 -0400

    Update Freudian_translator.rb

--- 8kyu/Freudian_translator.rb
+++ 8kyu/Freudian_translator.rb
@@ -5,3 +5,8 @@
 #Now, basically, when a person hears about Freud, s/he hears "sex" because for Freud, everything was basically related to, and explained by sex.
 #
 #In this kata, the toFreud() function will take a string as its argument, and return a string with every word replaced by the explanation to everything, according to Freud. Note that an empty string, or no arguments, should result in the ouput being ""(empty string).#
+
+
+def to_freud(sentence)
+  sentence.gsub(/(\S+)/, "sex")
+end
