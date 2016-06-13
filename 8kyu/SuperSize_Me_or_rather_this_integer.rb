df2409b1ea2eab5e3abd6be795ea1397a630af4f
JWNg <JWNg@users.noreply.github.com>
2016-06-13 07:55:24 -0400

    Update SuperSize_Me_or_rather_this_integer.rb

--- 8kyu/SuperSize_Me_or_rather_this_integer.rb
+++ 8kyu/SuperSize_Me_or_rather_this_integer.rb
@@ -7,3 +7,9 @@
 #super_size(12)     # 21
 #If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.#
 
+def super_size(n)
+  n = n.to_s.split(//).sort.reverse
+  big = ""
+  n.each {|i| big << i}
+  big.to_i
+end
