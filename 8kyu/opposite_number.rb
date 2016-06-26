c46e2aa0544dd157b01a24aa0f87e3028c605cbf
JWNg <JWNg@users.noreply.github.com>
2016-06-25 21:32:14 -0400

    Create opposite_number.rb

---
+++ 8kyu/opposite_number.rb
@@ -0,0 +1,14 @@
+#Description:
+#
+#Very simple, given a number, find its opposite.
+#
+#Examples:
+#
+#1: -1
+#14: -14
+#-34: 34
+#But can you do it in 1 line of code and without any conditionals?#
+
+def opposite(number)
+ number*-1
+end
