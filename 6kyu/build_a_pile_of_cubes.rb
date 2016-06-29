12306cfb8bc582b01ced0defdd0a103c2df557db
JWNg <JWNg@users.noreply.github.com>
2016-06-29 06:34:50 -0400

    Create build_a_pile_of_cubes.rb

---
+++ 6kyu/build_a_pile_of_cubes.rb
@@ -0,0 +1,23 @@
+#Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3, the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
+#
+#You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+#
+#The parameter of the function findNb (find_nb, find-nb) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
+#
+#Examples:
+#
+#findNb(1071225) --> 45
+#findNb(91716553919377) --> -1
+#
+
+def find_nb(m)
+  number_of_cubes = 0  
+  total = 0
+  until total > m do
+    #binding.pry
+    total = (0..number_of_cubes).inject {|total, n| total + (n**3)}
+    return number_of_cubes if total == m    
+    return -1 if total > m
+    number_of_cubes += 1
+  end  
+end
