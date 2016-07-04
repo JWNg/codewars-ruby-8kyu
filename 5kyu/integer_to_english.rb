e2f2428a4388da2f1c0b8bfae870dfef1d05ea8f
JWNg <JWNg@users.noreply.github.com>
2016-07-04 05:24:05 -0400

    Create integer_to_english.rb

---
+++ 5kyu/integer_to_english.rb
@@ -0,0 +1,27 @@
+##integer_to_english.rb
+#Description:
+#
+#For a given positive integer convert it into its English representation. All words are lower case and are separated with one space. No trailing spaces are allowed.
+#
+#To keep it simple, hyphens and the writing of the word 'and' both aren't enforced. (But if you are looking for some extra challenge, such an output will pass the tests.)
+#
+#Large number reference: http://en.wikipedia.org/wiki/Names_of_large_numbers (U.S., Canada and modern British)
+#
+#Input range: 1 -> 10**26 (10**16 for JS)
+#
+#Examples:
+#
+#int_to_english(1) == 'one'
+#
+#int_to_english(10) == 'ten'
+#
+#int_to_english(25161045656) == 'twenty five billion one hundred sixty one million forty five thousand six hundred fifty six'
+#or
+#
+#int_to_english(25161045656) == 'twenty five billion one hundred sixty-one million forty-five thousand six hundred and fifty-six'
+
+# return the English representation of the given number
+# int_to_english(1) => 'one'
+def int_to_english(number)
+  
+end
