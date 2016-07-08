d56008fedd3818079902d4b91cbc340e417a57c8
JWNg <JWNg@users.noreply.github.com>
2016-07-08 07:02:10 -0400

    Create tortoise_racing.rb

---
+++ 6kyu/tortoise_racing.rb
@@ -0,0 +1,25 @@
+#tortoise_racing.rb
+#Description:
+#
+#Two tortoises named A and B must run a race. A starts with an average speed of 720 feet per hour. Young B knows she runs faster than A and furthermore has not finished her cabbage.
+#
+#When she starts, at last, she can see that A has a 70 feet lead but B speed is 850 feet per hour. How long will it take B to catch A?
+#
+#More generally: given two speeds v1 (A speed, integer > 0) and v2 (B speed, integer > 0) and a lead g (integer > 0) how long will it take B to catch A?
+#
+#The result will be an array [h, mn, s] where h, mn, s is the time needed in hours, minutes and seconds (don't worry for fractions of second). If v1 >= v2 then return nil, nothing, null, None or {-1, -1, -1} for C++.
+#
+#Examples:
+#
+#race(720, 850, 70) => [0, 32, 18]
+#race(80, 91, 37) => [3, 21, 49]
+#Note: you can see some other examples in "Your test cases".
+# 
+def race(v1, v2, g)
+  return nil if (!v1 || !v2 || !g) || (v1 >= v2)
+  def secondnify (time)
+    time*(1/3600.0)
+  end
+  time_required = g/(secondnify(v2) - secondnify(v1))
+  [(time_required/3600).to_i, ((time_required%3600.0)/60).to_i, (time_required%60.0).to_i]
+end
