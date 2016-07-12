d5a691bdfddd7d0e9100469f75c166fe3063bd59
JWNg <JWNg@users.noreply.github.com>
2016-07-12 07:26:57 -0400

    Create Find_Maximum_and_Minimum_Values_of_a_List.rb

---
+++ 8kyu/Find_Maximum_and_Minimum_Values_of_a_List.rb
@@ -0,0 +1,13 @@
+#Description:
+#Find_Maximum_and_Minimum_Values_of_a_List
+#Your task is to make two functions, max and min that take a vector of integers list as input and outputs, respectively, the largest and lowest number in that vector.
+#
+#Examples
+#
+#max([4,6,2,1,9,63,-134,566]) returns 566
+#min([-52, 56, 30, 29, -54, 0, -110]) returns -110
+#max([5]) returns 5
+#min([42, 54, 65, 87, 0]) returns 0
+#Notes
+#
+#You may consider that there will not be any empty vectors.#
